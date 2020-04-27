fc59ec433ed4da556783ebc7b215a4dbc9d394c0
Brant Unger <brant.unger@cvshealth.com>
2020-04-26 18:47:29 -0700

    Format and remove imports

--- src/app/authentication-interceptor.ts
+++ src/app/authentication-interceptor.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { map, retry, catchError } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 import { MatDialog } from '@angular/material/dialog';
 import { LoginDialogComponent } from './login-dialog/login-dialog.component';
 import { AuthorizationService } from './services/authorization.service';
@@ -9,19 +9,21 @@ import { AuthorizationService } from './services/authorization.service';
 
 @Injectable()
 export class AuthenticationInterceptor implements HttpInterceptor {
-  constructor(private dialog: MatDialog, private authorizationService: AuthorizationService) {}
+  constructor(private dialog: MatDialog, private authorizationService: AuthorizationService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(req).pipe(
-      catchError((error: HttpErrorResponse) => {
-        if (error.status === 401) {
-          this.authorizationService.logout();
-          this.dialog.open(LoginDialogComponent, {
-            width: '400px'
-          });
-        }
-        return throwError(error);
-      })
-    );
+    return next
+      .handle(req)
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          if (error.status === 401) {
+            this.authorizationService.logout();
+            this.dialog.open(LoginDialogComponent, {
+              width: '400px'
+            });
+          }
+          return throwError(error);
+        })
+      );
   }
 }
