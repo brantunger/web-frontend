5273e8c4e3cf3f174d2bb07bc7f91788248a2e69
Brant Unger <brantunger@gmail.com>
2023-01-02 16:02:27 -0700

    Add menu to news story

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,39 @@
 import { CommonModule } from '@angular/common';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { JwtModule } from '@auth0/angular-jwt';
-import { AlertBannerComponent } from './components/alert-banner/alert-banner.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlertBannerComponent } from './components/alert-banner/alert-banner.component';
 import { ExpandCollapseComponent } from './components/expand-collapse/expand-collapse.component';
 import { LoginDialogComponent } from './components/login-dialog/login-dialog.component';
+import { NewsCardComponent } from './components/news-card/news-card.component';
+import { NewsContainerComponent } from './components/news-card/news-container/news-container.component';
 import { ShoutBoxComponent } from './components/shout-box/shout-box.component';
 import { SignupDialogComponent } from './components/signup-dialog/signup-dialog.component';
 import { TopNavigationComponent } from './components/top-navigation/top-navigation.component';
-import { NewsCardComponent } from './components/news-card/news-card.component';
-import { NewsContainerComponent } from './components/news-card/news-container/news-container.component';
 // import { AuthenticationInterceptor } from './authentication-interceptor';
-import { NoSantizingHtmlPipe } from './pipes/no-santizing-html.pipe';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { HomePageComponent } from './components/home-page/home-page.component';
-import { CreateNewsDialogComponent } from './components/create-news-dialog/create-news-dialog.component';
 import { EditorModule } from '@tinymce/tinymce-angular';
+import { CreateNewsDialogComponent } from './components/create-news-dialog/create-news-dialog.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 import { NewsPageComponent } from './components/news-page/news-page.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { FullFormattedTimestampPipe } from './pipes/full-formatted-timestamp.pipe';
-import { WebsocketMessagingService } from './services/websocket-messaging.service';
+import { NoSantizingHtmlPipe } from './pipes/no-santizing-html.pipe';
 import { websocketMessagingServiceFactory } from './services/rx-stomp-service-factory';
+import { WebsocketMessagingService } from './services/websocket-messaging.service';
 
 @NgModule({
   declarations: [
@@ -66,6 +67,7 @@ import { websocketMessagingServiceFactory } from './services/rx-stomp-service-fa
     MatCardModule,
     MatInputModule,
     MatIconModule,
+    MatMenuModule,
     MatDialogModule,
     MatDividerModule,
     HttpClientModule,
