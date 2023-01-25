9b2785c7685ed00df095245e139977816d1d7846
Brant Unger <brant.unger@cvshealth.com>
2023-01-25 16:43:47 -0700

    Add recursive comment count function

--- src/app/components/news-page/news-page.component.ts
+++ src/app/components/news-page/news-page.component.ts
@@ -2,7 +2,7 @@ import {HttpErrorResponse} from '@angular/common/http';
 import {Component, OnInit} from '@angular/core';
 import {Title} from '@angular/platform-browser';
 import {ActivatedRoute, ParamMap} from '@angular/router';
-import {switchMap, take} from 'rxjs';
+import {switchMap} from 'rxjs';
 import {News} from 'src/app/models/News';
 import {NewsComment} from 'src/app/models/NewsComment';
 import {WebApiService} from 'src/app/services/web-api.service';
@@ -41,5 +41,26 @@ export class NewsPageComponent implements OnInit {
         },
         error: (error: HttpErrorResponse) => console.log(error.error.error)
       });
+  }
+
+  displayDescription(): string {
+    if (!this.comments || this.comments.length === 0) {
+      return 'There are no comments'
+    } else {
+      return `There are ${this.getCommentCount(this.comments)} comments`
+    }
+  }
+
+  getCommentCount(commentsArr: NewsComment[]): number {
+    if (!commentsArr) return 0;
+    let commentCount: number = commentsArr.length;
+    for (let i = 0; i < commentsArr.length; i++) {
+      let subComments = commentsArr[i].comments;
+      if (!subComments) {
+        continue;
+      }
+      commentCount += this.getCommentCount(subComments);
+    }
+    return commentCount;
   }
 }
