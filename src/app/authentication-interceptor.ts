f6fdd05f1e4b207e07031f986864de13e71ae402
Brant Unger <brant.unger@cvshealth.com>
2023-01-31 02:25:20 -0700

    Add authentication intercept functionality

--- src/app/authentication-interceptor.ts
+++ src/app/authentication-interceptor.ts
@@ -9,25 +9,27 @@ import {
 } from "@angular/common/http";
 import {AuthorizationService} from "./services/authorization.service";
 import {Observable, tap} from "rxjs";
+import {environment} from "../environments/environment";
+import {Router} from "@angular/router";
 
 @Injectable()
 export class AuthenticationInterceptor implements HttpInterceptor {
 
-  constructor(private authorizationService: AuthorizationService) {
+  constructor(private router: Router) {
   }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
       tap({
         next: (event) => {
-          if (event instanceof HttpResponse) {
+          if (event instanceof HttpResponse && !environment.production) {
             console.log(`${request.method} request to ${request.url} returned ${event.status}`);
           }
         },
         error: (error: HttpErrorResponse) => {
           console.error('Error has occurred in HTTP Request', error.error);
           if (error.status === 401 || error.status === 403) {
-            // this.authorizationService.logout();
+            this.router.navigateByUrl('auth');
           }
         }
       }));
